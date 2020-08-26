d2753f50287a96fc03047ca87998827279fbdfce
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-08-26 15:42:08 -0300

    Adicionado interpolação de estring para simbolo de real

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyHomePage extends StatelessWidget {
                               Border.all(color: Colors.purple[100], width: 2)),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
